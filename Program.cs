aed545f44fe9521273a572db97ce86bf486ecfde
Uros Karaleic <karaleicuu@gmail.com>
2025-06-28 16:04:54 +0200

    returning blank image works

--- Program.cs
+++ Program.cs
@@ -1,3 +1,9 @@
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Formats.Png;
+using SixLabors.ImageSharp.Drawing.Processing;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 var app = builder.Build();
@@ -8,12 +14,29 @@ if (!app.Environment.IsDevelopment())
 }
 app.UseHttpsRedirection();
 app.UseRouting();
-app.MapGet("/", Hello);
 app.MapRazorPages();
 
-async static Task<IResult> Hello()
+app.MapGet("/", TimeEntriesPage);
+app.MapGet("/image", TimeEntriesPieChart);
+
+async static Task<IResult> TimeEntriesPage()
 {
     return TypedResults.Ok("hello world");
+}
+
+async static Task<IResult> TimeEntriesPieChart()
+{
+    using var img = new Image<Rgba32>(600, 600);
+    img.Mutate(ctx => ctx.Fill(Color.Red));
+    
+    var memoryStream = new MemoryStream();
+    await img.SaveAsync(memoryStream, new PngEncoder());
+    memoryStream.Position = 0;
+    
+    return TypedResults.File(
+       fileContents:memoryStream.ToArray(),
+       contentType:"image/png"
+       );
 }
 
 app.Run();
