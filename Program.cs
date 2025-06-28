5c80176dd320e77a9155f977c636eb3795672a67
Uros Karaleic <karaleicuu@gmail.com>
2025-06-28 15:38:06 +0200

    remove .idea, add system drawing package

--- Program.cs
+++ Program.cs
@@ -1,11 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
-
+builder.Services.AddRazorPages();
 var app = builder.Build();
 
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
 }
+app.UseHttpsRedirection();
+app.UseRouting();
+app.MapGet("/", Hello);
+app.MapRazorPages();
 
+async static Task<IResult> Hello()
+{
+    return TypedResults.Ok("hello world");
+}
 
 app.Run();
